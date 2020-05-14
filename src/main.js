322a86344b92d14b3741a0dc774549513fdcd55c
d1y <chenhonzhou@gmail.com>
2020-05-14 23:45:52 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue'
-import Click from 'vue-spark'
 import App from './App.vue'
 import router from './router'
 import './plugins/vant.js'
-
-import { isPost } from '@/config'
 import store from './store'
-
-if (isPost) Vue.use(Click())
 
 Vue.config.productionTip = false
 
